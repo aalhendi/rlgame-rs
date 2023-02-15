c9cf459658e9dd0090e04f0148e9067a0a62f245
Abdulrazaq Alhendi <aalhendi@protonmail.com>
2023-02-15 15:11:54 +0300

    feat: add numpad and vim key support

--- src/player.rs
+++ src/player.rs
@@ -19,12 +19,13 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 }
 
 pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    use VirtualKeyCode::*;
     if let Some(key) = ctx.key {
         match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            Left | Numpad4 | H => try_move_player(-1, 0, &mut gs.ecs),
+            Right | Numpad6 | L => try_move_player(1, 0, &mut gs.ecs),
+            Up | Numpad8 | K => try_move_player(0, -1, &mut gs.ecs),
+            Down | Numpad2 | J => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
         };
     }
