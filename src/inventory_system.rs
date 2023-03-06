26e3a4c0544a37484ab98c2b455a835f4f8448ae
Abdulrazzaq Alhendi <dev@aalhendi.com>
2023-03-06 17:21:36 +0300

    fix: show unequip in gamelog

--- src/inventory_system.rs
+++ src/inventory_system.rs
@@ -278,16 +278,33 @@ impl<'a> System<'a> for ItemRemoveSystem {
         WriteStorage<'a, WantsToRemoveItem>,
         WriteStorage<'a, Equipped>,
         WriteStorage<'a, InBackpack>,
+        WriteExpect<'a, Gamelog>,
+        ReadExpect<'a, Entity>,
+        ReadStorage<'a, Name>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (entities, mut wants_remove, mut equipped, mut backpack) = data;
+        let (
+            entities,
+            mut wants_remove,
+            mut equipped,
+            mut backpack,
+            mut gamelog,
+            player_entity,
+            names,
+        ) = data;
 
         for (entity, to_remove) in (&entities, &wants_remove).join() {
             equipped.remove(to_remove.item);
             backpack
                 .insert(to_remove.item, InBackpack { owner: entity })
                 .expect("Unable to insert backpack");
+            if entity == *player_entity {
+                gamelog.entries.push(format!(
+                    "You unequip the {item_name}.",
+                    item_name = names.get(to_remove.item).unwrap().name
+                ));
+            }
         }
 
         wants_remove.clear();
