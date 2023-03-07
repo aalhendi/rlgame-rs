9468df25bb84fffae830ed84d3aed35466502ffe
Abdulrazzaq Alhendi <dev@aalhendi.com>
2023-03-07 12:11:05 +0300

    feat: add CP437 walls

--- src/map.rs
+++ src/map.rs
@@ -230,6 +230,11 @@ impl Map {
             }
         }
     }
+
+    fn is_revealed_and_wall(&self, x: i32, y: i32) -> bool {
+        let idx = self.xy_idx(x, y);
+        self.tiles[idx] == TileType::Wall && self.revealed_tiles[idx]
+    }
 }
 
 pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
@@ -247,8 +252,8 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
                     fg = RGB::from_f32(0.0, 0.5, 0.5);
                 }
                 TileType::Wall => {
-                    glyph = rltk::to_cp437('#');
-                    fg = RGB::from_f32(0.0, 1.0, 0.0);
+                    glyph = get_wall_glyph(&*map, x, y);
+                    fg = RGB::from_f32(0., 1.0, 0.);
                 }
                 TileType::DownStairs => {
                     glyph = rltk::to_cp437('>');
@@ -267,5 +272,46 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk) {
             x = 0;
             y += 1;
         }
+    }
+}
+
+fn get_wall_glyph(map: &Map, x: i32, y: i32) -> rltk::FontCharType {
+    if x < 1 || x > map.width - 2 || y < 1 || y > map.height - 2 as i32 {
+        return 35;
+    }
+    let mut mask: u8 = 0;
+
+    if map.is_revealed_and_wall(x, y - 1) {
+        mask += 1;
+    }
+    if map.is_revealed_and_wall(x, y + 1) {
+        mask += 2;
+    }
+    if map.is_revealed_and_wall(x - 1, y) {
+        mask += 4;
+    }
+    if map.is_revealed_and_wall(x + 1, y) {
+        mask += 8;
+    }
+
+    // Uses <http://dwarffortresswiki.org/index.php/Character_table>
+    match mask {
+        0 => 9,    // Pillar because we can't see neighbors
+        1 => 186,  // Wall only to the north
+        2 => 186,  // Wall only to the south
+        3 => 186,  // Wall to the north and south
+        4 => 205,  // Wall only to the west
+        5 => 188,  // Wall to the north and west
+        6 => 187,  // Wall to the south and west
+        7 => 185,  // Wall to the north, south and west
+        8 => 205,  // Wall only to the east
+        9 => 200,  // Wall to the north and east
+        10 => 201, // Wall to the south and east
+        11 => 204, // Wall to the north, south and east
+        12 => 205, // Wall to the east and west
+        13 => 202, // Wall to the east, west, and south
+        14 => 203, // Wall to the east, west, and north
+        15 => 206, // ╬ Wall on all sides
+        _ => 35,   // Fallthrough...
     }
 }
