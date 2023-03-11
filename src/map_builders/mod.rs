dc99ee1d55c614124c885ed349d38067263f3d03
Abdulrazzaq Alhendi <dev@aalhendi.com>
2023-03-11 16:58:21 +0300

    feat: add BSP interior map builder

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -3,6 +3,8 @@ mod simple_map;
 use simple_map::SimpleMapBuilder;
 mod bsp_dungeon;
 use bsp_dungeon::BspDungeonBuilder;
+mod bsp_interior;
+use bsp_interior::BspInteriorBuilder;
 mod common;
 use specs::World;
 
@@ -17,9 +19,10 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 2);
+    let builder = rng.roll_dice(1, 3);
     match builder {
         1 => Box::new(SimpleMapBuilder::new(new_depth)),
-        _ => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(BspInteriorBuilder::new(new_depth)),
     }
 }
